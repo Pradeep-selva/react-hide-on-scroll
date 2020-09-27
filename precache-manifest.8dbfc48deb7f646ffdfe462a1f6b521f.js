9340c7c6f8b7addb235a769a0b3db8f3dd2def14
Pradeep-selva <pradeepselvaraju@gmail.com>
2020-09-28 05:11:55 +0530

    Updates

---
+++ precache-manifest.8dbfc48deb7f646ffdfe462a1f6b521f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85e9c52aa06305502f4f217fbc97ad2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e3fcea3583710804f54",
+    "url": "./static/css/main.62046bd0.chunk.css"
+  },
+  {
+    "revision": "8649439f83fcc4c760e7",
+    "url": "./static/js/2.8eb51eb2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.8eb51eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3fcea3583710804f54",
+    "url": "./static/js/main.591d8c25.chunk.js"
+  },
+  {
+    "revision": "67f0391474484bbb0801",
+    "url": "./static/js/runtime-main.a376ba55.js"
+  }
+]);
